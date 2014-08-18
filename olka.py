16262d08337f93c0186536c85c75c3801d9e67ab
Marko Poutiainen <Marko.Poutiainen@elektrobit.com>
2014-08-18 13:31:44 +0300

    Refactored code.

--- olka.py
+++ olka.py
@@ -7,57 +7,42 @@ import csv
 ''' Reverse name: Get last part of name and move it to beginning '''
 reverse = lambda name: name.split().pop() + ' ' + ' '.join([v for v in name.split()[:-1]])
 
-def open_cd(file_name):
-    '''
-    Open CableDesigner CSV
-    :param file_name:  Name of csv file
-    :return: List of tuples of names found. Tuple has name & reversed name
-    '''
-    retval = []
+'''
+Open CableDesigner CSV
+:param file_name:  Name of csv file
+:return: List of tuples of names found. Tuple has name & reversed name
+'''
+open_cd = lambda file_name: [(x[0], reverse(x[0])) for x in csv.reader(open(file_name), delimiter=';')]
 
-    f = open(file_name)
-    reader = csv.reader(f, delimiter=';')
-    for line in reader:
-        # Name is in first column
-        retval.append((line[0], reverse(line[0])))
-    return retval
+'''
+Name is in third column & has a number in front of it that needs to be removed.
+'''
+doora_extract = lambda s: ' '.join(s[2].split()[1:])
 
-def open_doora(file_name):
-    '''
-    Open DooraNet CSV
-    :param file_name:  Name of csv file
-    :return: List of tuples of names found. Tuple has name & reversed name
-    '''
-    retval =[]
-
-    f = open(file_name)
-    reader = csv.reader(f, delimiter=';')
-    for line in reader:
-        if line[0][0:4] == 'LASK':
-            # Name is in third column & has a number in front of it that
-            # needs to be removed
-            name = ' '.join(line[2].split()[1:])
-            retval.append((name, reverse(name)))
-    return retval
+'''
+Open DooraNet CSV
+:param file_name:  Name of csv file
+:return: List of tuples of names found. Tuple has name & reversed name
+'''
+open_doora = lambda file_name:  [(doora_extract(x), reverse(doora_extract(x)))
+                                 for x in csv.reader(open(file_name), delimiter=';') if x[0][0:4] == 'LASK']
 
 s = open_doora(sys.argv[2])
 s2 = open_cd(sys.argv[1])
 
-doora =[]
+doora = set()
 # Following compares unreversed names from dooranet file to reversed
 # file names. This is done by creating sets from both lists of names
 # and doing a difference operation to them.
 for i in (set([i[0] for i in s])-set([i[1] for i in s2])-set(i[0] for i in s2)):
-    if i not in doora:
-        doora.append(i)
+    doora.add(i)
 
-cable =[]
+cable =set()
 # Following compares unreversed names from cable designer to reversed
-# names from dooanet.
+# names from dooranet.
 for i in (set([i[0] for i in s2])-set([i[1] for i in s])-set(i[0] for i in s)):
-    if i not in cable:
-        cable.append(i)
-
+    cable.add(i)
+#cable = []
 print "Puuttuvat Doorasta"
 
 for i in sorted(doora):
